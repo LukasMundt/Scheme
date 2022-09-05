e9db1eb14466403cf54f49e1c6ed36fddd03c26f
LukasMundt <91488807+LukasMundt@users.noreply.github.com>
2022-09-05 12:57:09 +0200

    Update mehrfachverzweigung.rkt

--- mehrfachverzweigung.rkt
+++ mehrfachverzweigung.rkt
@@ -13,3 +13,5 @@
 
 (plus 1 3) ;--> 4
 (plus 1 'hund) ;--> error: b ist keine Zahl
+(plus 'hund 1) ;--> error: a ist keine Zahl
+(plus 'hund 'katze) ;--> error: a ist keine Zahl (Abarbeitung in Reihung wird deutlich)
