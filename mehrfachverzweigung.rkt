d6b3f8a5d142a52d6252a6f4451e624d1af9b54d
LukasMundt <91488807+LukasMundt@users.noreply.github.com>
2022-09-05 12:45:07 +0200

    Add files via upload

---
+++ mehrfachverzweigung.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define
+  (plus a b)
+  (cond
+    ((not(number? a)) (error "a:" a "ist keine Zahl"))
+    ((not(number? b)) (error "b:" b "ist keine Zahl"))
+    (else
+     (+ a b)
+     )
+    )
+  )
+
+(plus 1 3) ;--> 4
+(plus 1 'hund) ;--> error: b ist keine Zahl
